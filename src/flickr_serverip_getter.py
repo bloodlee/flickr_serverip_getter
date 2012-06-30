48031ebd64cf02db694ad828c01c279f53895817
Yongqiang Li <jasonleehust@gmail.com>
2012-06-30 20:57:29 +0800

    Initial Commit

---
+++ src/flickr_serverip_getter.py
@@ -0,0 +1,15 @@
+"""
+In China, sometimes we can't access the "Global" servers of flickr.
+In this case, we need to use East servers of flickr instead.
+
+This script is used to get all East servers' ip addresses.
+And then we can use these ip addresses in /etc/hosts.
+
+Need at least Python 2.7.
+"""
+
+# import argparse
+
+if __name__ == '__main__':
+
+    pass
